941e2ac824236120cae49d0d259df27c5f0d68ca
dawidbladek0831 <dawidbladek0831@gmail.com>
2025-07-01 22:33:55 +0200

    save

--- .kubernetes/base/env/gke-cluster.tf
+++ .kubernetes/base/env/gke-cluster.tf
@@ -29,7 +29,7 @@ resource "google_container_node_pool" "gke_nodes" {
   node_count = 2
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-highcpu-8"
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
